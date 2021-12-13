8114cddfaa43b8e0ffcbaeffb98756883b16686c
KosSashaHelloWorld <74153083+KosSashaHelloWorld@users.noreply.github.com>
2021-12-13 04:07:59 +0300

    Interface -> Abstract class

--- src/main/java/by/kosolobov/task4/entity/packages/BasePackage.java
+++ src/main/java/by/kosolobov/task4/entity/packages/BasePackage.java
@@ -1,12 +1,52 @@
 package by.kosolobov.task4.entity.packages;
 
 import by.kosolobov.task4.entity.state.BasePackageState;
+import by.kosolobov.task4.entity.state.impl.UnorderedState;
 
-public interface BasePackage {
-    void printStatus();
-    void nextState();
-    void prevState();
-    void setState(BasePackageState newState);
-    BasePackageState getState();
-    int getWeight();
+public abstract class BasePackage {
+    protected final int weight;
+    protected BasePackageState state = new UnorderedState();
+
+    protected BasePackage(int weight) {
+        this.weight = weight;
+    }
+
+    public void printStatus() {
+        state.printStatus();
+    }
+
+    public void nextState() {
+        state.next(this);
+    }
+
+    public void prevState() {
+        state.prev(this);
+    }
+
+    public void setState(BasePackageState newState) {
+        state = newState;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public BasePackageState getState() {
+        return state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BasePackage that = (BasePackage) o;
+
+        return weight == that.weight;
+    }
+
+    @Override
+    public int hashCode() {
+        return weight;
+    }
 }
